546ab6586474258ec5ae3cd76504f3bf9ba12ade
jonathan_geovany <hv12i04001@usonsonate.edu.sv>
2018-10-12 18:05:03 -0600

    bd actualizada

--- database/bd_contratado.sql
+++ database/bd_contratado.sql
@@ -1,122 +1,116 @@
 create database contratado;
 
+use contratado;
+
+-- estado de un comentario
 create table cCommentState(
-id int(11) primary key auto_increment,
-state varchar(250)
+	id             	int(11) primary key auto_increment,
+	state        	varchar(250) -- mal servicio , servicio regular , buen servicio , excelente servicio
 );
 
+-- estado de un acuerdo
 create table cAgreeState(
-id int(11) primary key auto_increment,
-state varchar(250)
+	id             	int(11) primary key auto_increment,
+	state        	varchar(250) -- rechazado , pendiente , aceptado
 );
 
-
-create table cCountry(
-id int(11) primary key auto_increment,
-country_name varchar(200) unique,
-country_code varchar(50) unique,
-country_iso varchar(50) unique
-);
-
-create table cCity(
-id int(11) primary key auto_increment,
-city_name varchar(200),
-id_country int(11),
-constraint fk_id_country foreign key (id_country) references cCountry(id)
-);
-
+-- categoria de un trabajo
 create table cCategory(
-id int(11) primary key auto_increment,
-category_name varchar(200)
+	id             	int(11) primary key auto_increment,
+	category_name varchar(200)
 );
 
+-- subcategoria de un trabajo
 create table cSubCategory(
-id int(11) primary key auto_increment,
-subcategory_name varchar(200),
-id_category int(11),
-constraint fk_id_category foreign key (id_category) references cCategory(id)
+	id             	int(11) primary key auto_increment,
+	subcategory_name varchar(200),
+	id_category int(11),
+	constraint fk_id_category foreign key (id_category) references cCategory(id) on delete cascade
+);
+
+-- usuario
+create table cUser(
+	id 				int(11) primary key auto_increment,
+	first_name 	varchar(250) not null,
+	last_name 	varchar(250),
+	id_number 	varchar(250),
+	phone			varchar(200),
+	email 			varchar(250),
+    birthday 		date,
+	country_code  	varchar(250),  -- Ejemplo SV
+	country_name 	varchar(250),  -- Ejemplo El Salvador
+	country_state  	varchar(250),  -- Ejemplo Sonsonate
+    country_city			varchar(250),  -- Ejemplo Nahuizalco
+	user_info			 	varchar(250),  -- informacion sobre el usuario que desea compartir
+    picture_path 		varchar(250), --  por el momento omitir para datos de prueba
+	document_path 	varchar(250), --  por el momento omitir para datos de prueba
+    latitude                double,          -- utilizar datos reales
+    longitude             double,          -- utilizar datos reales
+    is_an_advertiser	bool
+);
+
+create table cUserSubCategory(
+	id 						int(11) primary key auto_increment,
+    id_user				int(11),
+    id_subcategory	int(11),
+    constraint fk_usersubcat_user	foreign key (id_user) 				references cUser(id),
+    constraint fk_usersubcat_cat 		foreign key (id_subcategory)	references cSubCategory(id)
+);
+
+create table cRate(
+	id int(11) 		primary key auto_increment,
+	rate 				decimal,
+    id_user		int(11),
+	constraint fk_rate_user foreign key (id_user) references cUser(id)
 );
 
 create table cExperience(
-id int(11) primary key auto_increment,
-employer varchar(250),
-occupation varchar(250),
-until_date date
+	id int(11) 		primary key auto_increment,
+	employer 	varchar(250),
+	occupation 	varchar(250),
+	exp_date 	date,
+    id_user		int(11),
+    constraint fk_exp_user foreign key (id_user) references cUser(id)
 );
 
 create table cReference(
-id int(11) primary key auto_increment,
-first_name varchar(250),
-occupation varchar(250),
-phone_number varchar(250),
-email varchar (250),
-id_experience int(11),
-constraint fk_id_experience foreign key (id_experience) references cReference(id)
-);
-
-create table cUser(
-id int(11) primary key auto_increment,
-first_name varchar(250) not null,
-last_name varchar(250),
-id_number varchar(250),
-birthday date,
-phone_number varchar(200),
-email varchar(250),
-address varchar(200),
-is_an_ad bool,
-picture_path varchar(250),
-document_path varchar(250),
-occupation_info varchar(250),
-id_subcategory int(11),
-id_city int(11),
-constraint fk_id_subcategory foreign key (id_subcategory) references cSubCategory(id),
-constraint fk_id_city foreign key (id_city) references cCity(id)
-);
-
-create table cComplaint(
-id int(11) primary key auto_increment,
-details text not null,
-id_user int,
-date_occur date,
-constraint fk_id_user_complaint foreign key (id_user) references cUser(id)
-);
-
-create table cUserExperience(
-id int(11) primary key auto_increment,
-id_experience int(11),
-id_user int(11),
-constraint fk_id_exp  foreign key (id_experience) references cExperience(id),
-constraint fk_id_user foreign key (id_user) references cUser(id)
+	id int(11) 		primary key auto_increment,
+	first_name 	varchar(250),
+	occupation 	varchar(250),
+	phone			varchar(250),
+	email 			varchar(250),
+	id_exp			int(11),
+	constraint fk_ref_exp foreign key (id_exp) references cExperience(id)
 );
 
 create table cAgreement(
-id int(11) primary key auto_increment,
-id_employer int(11) unique,
-id_worker int(11) unique,
-work_details varchar(250),
-date_agree date,
-id_agreestate int(11),
-constraint fk_id_employer  foreign key (id_employer) references cUser(id),
-constraint fk_id_worker foreign key (id_worker) references cUser(id),
-constraint fk_agreestate foreign key (id_agreestate) references cAgreeState(id)
+	id 					int(11) primary key auto_increment,
+	job_details 		varchar(250),
+	date_agree 		date,
+	id_agreestate	int(11),
+    id_employer 	int(11),
+	id_employee 	int(11),
+	constraint fk_agree_state 			foreign key (id_agreestate) references cAgreeState(id),
+	constraint fk_agree_employer  	foreign key (id_employer) 	references cUser(id),
+	constraint fk_agree_employee 	foreign key (id_employee) 	references cUser(id)
 );
 
-
-create table cRate(
-id int(11) primary key auto_increment,
-rate decimal,
-votes int(11),
-id_user int(11),
-constraint fk_id_user_rate foreign key (id_user) references cUser(id)
+create table cComplaint(
+	id 					int(11) primary key auto_increment,
+	details 				text not null,
+	date_occur 		date,
+	id_employer 	int(11), -- empleador patron
+    id_employee	int(11), -- empleado trabajador
+	constraint fk_complaint_employer	foreign key (id_employer) 	references cUser(id),
+    constraint fk_complaint_employee	foreign key (id_employee) 	references cUser(id)
 );
 
 create table cComment(
-id int(11) primary key auto_increment,
-commentary text,
-id_state int(11),
-id_user int(11),
-id_agree int(11),
-constraint fk_id_state  foreign key (id_state) references cCommentState(id),
-constraint fk_comment_user  foreign key (id_user) references cUser(id),
-constraint fk_agree_comment  foreign key (id_agree) references cAgreement(id)
+	id 					int(11) primary key auto_increment,
+	commentary 	text,
+    id_user 			int(11),
+	id_comment_state 	int(11),
+	comment_date 		datetime,
+	constraint fk_comment_user			foreign key (id_user) references cUser(id),
+    constraint fk_comment_state  		foreign key (id_comment_state) references cCommentState(id)
 );
--- database/init_data.sql
+++ database/init_data.sql
@@ -2,14 +2,46 @@
 
 use contratado;
 
-insert into cCommentState (state) value ('Pesimo servicio');
-insert into cCommentState (state) value ('Servicio regular');
-insert into cCommentState (state) value ('Excelente servicio');
-
-insert into cAgreeState (state) value ('Petición enviada');
-insert into cAgreeState (state) value ('Petición aceptada');
-insert into cAgreeState (state) value ('Petición rechazada');
 
 
+insert into cCommentState value (1,'Pesimo servicio');
+insert into cCommentState value (2,'Servicio regular');
+insert into cCommentState value (3,'Excelente servicio');
 
+insert into cAgreeState value (1,'Petición enviada');
+insert into cAgreeState value (2,'Petición aceptada');
+insert into cAgreeState value (3,'Petición rechazada');
+
+insert into cCategory value (1,'Administrativo');
+insert into cCategory value (2,'Agrario');
+insert into cCategory value (2,'Alimentos y servicios');
+insert into cCategory value (3,'Artístico');
+insert into cCategory value (4,'Cine y teatro');
+insert into cCategory value (5,'Comunicación');
+insert into cCategory value (6,'Derecho');
+insert into cCategory value (7,'Economía y empresas');
+insert into cCategory value (8,'Estética');
+insert into cCategory value (9,'Humanidades');
+insert into cCategory value (10,'Investigación científica');
+insert into cCategory value (11,'Ingeniería y técnicos');
+insert into cCategory value (12,'Música');
+insert into cCategory value (13,'Oficios');
+insert into cCategory value (14,'Proteción y seguridad');
+insert into cCategory value (15,'Sanidad');
+
+insert into cSubCategory value (1,' Adminstrativo Comercial',1);
+insert into cSubCategory value (2,' Adminstrativo Bancario',1);
+insert into cSubCategory value (3,' Gestión de personal',1);
+insert into cSubCategory value (4,' Analista de datos',1);
+insert into cSubCategory value (5,' Secretario/a',1);
+
+
+
+
+select * from cCountry c, cState s where country_name="El Salvador" and c.id = s.id_country ;
+
+select * from cCity where id_state = 1089;
+
+
+select subcat.id , subcat.subcategory_name , subcat.id_category,cat.category_name from cSubCategory subcat, cCategory cat where cat.id=subcat.id_category;
 
